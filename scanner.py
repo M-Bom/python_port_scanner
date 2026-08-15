ba7fcfbf886cb8845667496d72bddcd253c8fa2a
M-Bom <150365129+M-Bom@users.noreply.github.com>
2026-08-14 21:08:21 -0600

    Update scanner.py
    Added function that scans multiple ports using a thread pool. Adds the open ports to a list and prints how long it took to scan and returns the list of open ports.

--- scanner.py
+++ scanner.py
@@ -25,3 +25,33 @@ def scan_port(target: str, port: int, timeout: float = 1.0):
     # Close TCP socket
     finally:
         sock.close()
+
+def scan_ports_threaded(target: str, ports: range, max_workers: int = 100):
+    """
+    Scan multiple ports using a thread pool.
+    """
+    open_ports = []
+    start_time = time.time()
+
+    # scan = partial(scan_port, target)
+
+    # Start a scan for every port and keep track of which port belongs to which scan
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(scan_port, target, port): port for port in ports}
+
+        # Get each scan result as they finish and then check if the result is valid
+        # if yes add to open port list 
+        for future in as_completed(futures):
+            port = futures[future]
+            try:
+                if future.result():
+                    open_ports.append(port)
+
+            # Fail safe for if an erorr occurs to not abort the scan
+            except:
+                pass
+
+    # Check how long the san took
+    elapsed = time.time() - start_time
+    print(f"\nScan completed in {elapsed:.2f} seconds")
+    return open_ports
